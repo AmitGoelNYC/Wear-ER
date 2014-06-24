c201b2b71e8df2b79fefe366389610736778e96c
Markypc3 <Markypc3@gmail.com>
2014-06-24 01:47:24 -0400

    proto V0.1.2 demo concept

--- ER/src/com/example/er/MainActivity.java
+++ ER/src/com/example/er/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.preview.support.v4.app.NotificationManagerCompat;
 import android.preview.support.wearable.notifications.WearableNotifications;
@@ -23,6 +25,9 @@ import android.support.v4.app.NotificationCompat.BigTextStyle;
 public class MainActivity extends Activity {
 
 	Spinner patspinner;
+	String input;
+	Spinner spinner;
+	EditText edittext;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +46,9 @@ public class MainActivity extends Activity {
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		patspinner.setAdapter(adapter);
 		
-		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+		edittext = (EditText) findViewById(R.id.edittext);
+		
+		spinner = (Spinner) findViewById(R.id.spinner1);
 		ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
 				this, R.array.plan_array,
 				android.R.layout.simple_spinner_item);
@@ -58,55 +65,60 @@ public class MainActivity extends Activity {
 	}
 
 	protected void sendMessage() {
+		String input = edittext.getText().toString();
+		Log.i("yes", input);
+		if(!input.equals("")){
+			int notificationId = 001;
+			// Build intent for notification content
+			Intent viewIntent = new Intent(this, MainActivity.class);
+			viewIntent.putExtra("jj", 2);
+			PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0,
+					viewIntent, 0);
 
-		int notificationId = 001;
-		// Build intent for notification content
-		Intent viewIntent = new Intent(this, MainActivity.class);
-		viewIntent.putExtra("jj", 2);
-		PendingIntent viewPendingIntent = PendingIntent.getActivity(this, 0,
-				viewIntent, 0);
+			// Specify the 'big view' content to display the long
+			// event description that may not fit the normal content text.
+			BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
+			bigStyle.bigText("yes");
 
+			String patient = patspinner.getSelectedItem().toString();
+			
+			// Create builder for the main notification
+			NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
+					this)
+					.setSmallIcon(R.drawable.ic_launcher)
+					.setContentTitle("Patient")
+					.setContentText(
+							patient
+									+ " in Critical Care needs Urgent injection of .. 30 cc's")
+					.setContentIntent(viewPendingIntent)
+					.setLargeIcon(
+							BitmapFactory.decodeResource(getResources(),
+									R.drawable.rsz_darkblu));
+
+			// Create a big text style for the second page
+			BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
+			secondPageStyle.setBigContentTitle("Details").bigText(
+					/*"Vitals  BP - 140/100.  HR - 145.  Currently bleeding internally."
+							+ "Requesting immediate assistance"*/
+					 input + " " + spinner.getSelectedItem().toString() + "?");
+
+			// Create second page notification
+			Notification secondPageNotification = new NotificationCompat.Builder(
+					this).setStyle(secondPageStyle).build();
+
+			// Create main notification and add the second page
+			Notification twoPageNotification = new WearableNotifications.Builder(
+					notificationBuilder).addPage(secondPageNotification).build();
+
+			// Get an instance of the NotificationManager service
+			NotificationManagerCompat notificationManager = NotificationManagerCompat
+					.from(this);
+
+			// Build the notification and issues it with notification manager.
+			notificationManager.notify(notificationId, twoPageNotification);
+		}
 		
-		// Specify the 'big view' content to display the long
-		// event description that may not fit the normal content text.
-		BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
-		bigStyle.bigText("yes");
 
-		String patient = patspinner.getSelectedItem().toString();;
-		// Create builder for the main notification
-		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(
-				this)
-				.setSmallIcon(R.drawable.ic_launcher)
-				.setContentTitle("Patient")
-				.setContentText(
-						 patient + " in Critical Care needs Urgent injection of .. 30 cc's")
-				.setContentIntent(viewPendingIntent)
-				.setLargeIcon(
-						BitmapFactory.decodeResource(getResources(),
-								R.drawable.rsz_darkblu));
-
-		// Create a big text style for the second page
-		BigTextStyle secondPageStyle = new NotificationCompat.BigTextStyle();
-		secondPageStyle.setBigContentTitle("Details").bigText(
-				"Vitals  BP - 140/100.  HR - 145.  Currently bleeding internally."
-						+ "Requesting immediate assistance");
-
-		// Create second page notification
-		Notification secondPageNotification = new NotificationCompat.Builder(
-				this).setStyle(secondPageStyle).build();
-
-		// Create main notification and add the second page
-		Notification twoPageNotification = new WearableNotifications.Builder(
-				notificationBuilder).addPage(secondPageNotification).build();
-
-		// Get an instance of the NotificationManager service
-		NotificationManagerCompat notificationManager = NotificationManagerCompat
-				.from(this);
-
-		// Build the notification and issues it with notification manager.
-		notificationManager.notify(notificationId, twoPageNotification);
-
-		
 	}
 
 	@Override
